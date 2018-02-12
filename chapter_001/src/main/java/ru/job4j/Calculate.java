fddd866623be4e23a43fbdc6fe216933f1ea2f85
00hadron <hadron@yandex.ru>
2018-02-12 15:24:17 +0300

    1.4. JUnit (к вопросу)

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+/**
+ * Calculate.
+ *
+ * @author epopova
+ * @since 09.12.2017
+ * @version 1
+ */   
+public class Calculate {
+	/**
+	 * main.
+	 * @param args - args.
+	 */
+	public static void main (String[] args){
+		System.out.println("Hello World!");
+	}
+	
+	/**
+	 * Method echo.
+	 * @param name - Your name.
+	 * @return - Echo plus your name.
+	 */
+	public String echo (String name) {
+		return ("Eсho, echo, echo :" + name);
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.ls.is;
+import static org.junit.AssertThat;
+
+/**
+ * Test.
+ *
+ * @author epopova
+ * @version $ld$
+ * @since 0.1
+ */
+public class CalculateTest {
+	/**
+	 * Test echo.
+	 */ @Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+	String input = "epopova";
+	String expect = "Echo, echo, echo : epopova";
+	Calculate calc = new Calculate();
+	String result = calc.echo(input);
+	assertThat(result,is(expect));
+	}
+}
