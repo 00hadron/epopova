02471d5670e15e161d5fe4b54c95e47e0cbb2bd9
00hadron <hadron@yandex.ru>
2018-02-14 10:47:09 +0300

    1.4. JUnit [#33500]

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -3,8 +3,8 @@ package ru.job4j;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import static org.hamcrest.core.ls.is;
-import static org.junit.AssertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Test.
