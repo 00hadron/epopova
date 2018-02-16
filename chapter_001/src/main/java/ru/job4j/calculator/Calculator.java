d3a0aa981560874ce7445b3db1e4e81dd2f85cd3
00hadron <hadron@yandex.ru>
2018-02-16 22:05:35 +0300

    3.1. Элементарный калькулятор.[#41590]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,62 @@
+package ru.job4j.calculator;
+
+
+/**
+ * Calculator.
+ *
+ * @author epopova
+ * @since 16.02.2018
+ * @version $Id$
+ */
+public class Calculator {
+    private double result;
+
+    /**
+     * Method add  - сложение двух чисел.
+     * @param first - первое число
+     * @param second - второе число
+     * неявный параметр result - сумма чисел
+     */
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    /**
+     * Method subtract  - вычитание двух чисел.
+     * @param first - первое число
+     * @param second - второе число
+     * неявный параметр result - разность чисел
+     */
+    public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+
+    /**
+     * Method div  - деление двух чисел.
+     * @param first - первое число
+     * @param second - второе число
+     * неявный параметр result - отношение чисел
+     */
+    public void div(double first, double second) {
+        this.result = second / first;
+    }
+
+    /**
+     * Method multiple  - произведение двух чисел.
+     * @param first - первое число
+     * @param second - второе число
+     * неявный параметр result - произведение чисел
+     */
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+
+    /**
+     * Method getResult.
+     * неявный параметр result
+     * @return result
+     */
+    public double getResult() {
+        return this.result;
+    }
+}
